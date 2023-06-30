784affde45b21f33b8b9b64578a8b3f5f2f19a96
Abdullah Çiftçi <abdullahciftci034@gmail.com>
2023-06-30 22:08:44 +0300

    git ignore and key generate yapildi

--- laravel_app/app/routes/web.php
+++ laravel_app/app/routes/web.php
@@ -15,8 +15,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello', function () {
-    return view("hello");
-});
-Route::get('/Merhaba','App\Http\Controllers\hello@index');
+Route::get('/hello','App\Http\Controllers\hello@index');
