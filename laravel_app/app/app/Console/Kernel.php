b1d1a34ccb446241e26203fbe4b6d7fc3dfdfa0f
Abdullah Çiftçi <abdullahciftci034@gmial.com>
2023-06-30 11:10:54 +0000

    first commit

--- laravel_app/app/app/Console/Kernel.php
+++ laravel_app/app/app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
     }
 
-    /**
+    /**sadasdasd
      * Register the commands for the application.
      *
      * @return void
